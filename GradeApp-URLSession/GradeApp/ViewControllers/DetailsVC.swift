96bf95c17cafdbfb516f47b45dedb728c79fd5dc
MehmetBilirr <mehmetbilir@aol.com>
2022-05-18 02:12:34 +0300

    update func added

--- GradeApp-URLSession/GradeApp/ViewControllers/DetailsVC.swift
+++ GradeApp-URLSession/GradeApp/ViewControllers/DetailsVC.swift
@@ -26,7 +26,10 @@ class DetailsVC: UIViewController {
     }
     
     @IBAction func updateClicked(_ sender: Any) {
-        
+        if let grade = chosenGrade,let name = lessonText.text,let g1 = grade1Text.text,let g2 = grade2Text.text {
+            update(grade_id: grade.not_id!, grade_name: name, grade1: g1, grade2: g2)
+            navigationController?.popViewController(animated: true)
+        }
         
         
     }
@@ -49,6 +52,27 @@ class DetailsVC: UIViewController {
         
         request.httpMethod = "POST"
         let postString = "not_id=\(grade_id)"
+        request.httpBody = postString.data(using: .utf8)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if error != nil || data == nil {
+                print(error?.localizedDescription)
+            }
+            do{
+                try JSONSerialization.jsonObject(with: data!)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    func update(grade_id:String,grade_name:String,grade1:String,grade2:String){
+        
+        
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/notlar/update_not.php")!)
+        
+        request.httpMethod = "POST"
+        let postString = "not_id=\(grade_id)&ders_adi=\(grade_name)&not1=\(grade1)&not2=\(grade2)"
         request.httpBody = postString.data(using: .utf8)
         URLSession.shared.dataTask(with: request) { data, response, error in
             
