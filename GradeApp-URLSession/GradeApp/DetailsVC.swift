29be5441f2ebf8418446b6c90150f8ac77bbb2ed
MehmetBilirr <mehmetbilir@aol.com>
2022-05-17 22:03:38 +0300

    init commit

---
+++ GradeApp-URLSession/GradeApp/DetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVC.swift
+//  GradeApp
+//
+//  Created by Mehmet Bilir on 10.05.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var grade2Text: UITextField!
+    @IBOutlet weak var grade1Text: UITextField!
+    @IBOutlet weak var lessonText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+
+}
