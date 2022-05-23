c9ab73daddb4c03e4611b24293a8d3a498a6e6c2
MehmetBilirr <mehmetbilir@aol.com>
2022-05-23 15:45:30 +0300

    alamofire init

---
+++ ContactsApp-Alamofire/ContactsApp/VC/UpdateContactVC.swift
@@ -0,0 +1,25 @@
+//
+//  UpdateContactVC.swift
+//  ContactsApp
+//
+//  Created by Mehmet Bilir on 12.05.2022.
+//
+
+import UIKit
+
+class UpdateContactVC: UIViewController {
+
+    @IBOutlet weak var contactNumberText: UITextField!
+    @IBOutlet weak var contactNameText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func updateClicked(_ sender: Any) {
+    }
+    
+
+}
