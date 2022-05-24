d88c5ac52687413d3f01a35b66d686b577c7254b
MehmetBilirr <mehmetbilir@aol.com>
2022-05-24 23:28:01 +0300

    movie details func added

--- MoviesApp-Firebase/MoviesApp/VC/DetailsVC.swift
+++ MoviesApp-Firebase/MoviesApp/VC/DetailsVC.swift
@@ -14,10 +14,17 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var movieYear: UILabel!
     @IBOutlet weak var movieName: UILabel!
     @IBOutlet weak var movieImageView: UIImageView!
+    var chosenMovie : Movies?
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let movie = chosenMovie {
+            movieDirector.text = movie.yonetmen
+            movieName.text = movie.film_ad
+            movieYear.text = movie.film_yil
+            movieCategory.text = movie.kategori
+            movieImageView.image = UIImage(named: movie.film_resim!)
+        }
     }
     
 
--- MoviesApp-Firebase/MoviesApp/VC/MoviesVC.swift
+++ MoviesApp-Firebase/MoviesApp/VC/MoviesVC.swift
@@ -13,6 +13,7 @@ class MoviesVC: UIViewController {
     var movieArray = [Movies]()
     @IBOutlet weak var collectionView: UICollectionView!
     var chosenCategory : Categories?
+    var chosenMovie : Movies?
     var firestoreDB = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -107,7 +108,17 @@ extension MoviesVC:UICollectionViewDelegate,UICollectionViewDataSource,MoviesPro
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        chosenMovie = movieArray[indexPath.row]
+        
         self.performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenMovie = chosenMovie
+            
+        }
     }
     
     
