6a1bf953fa56a3fc8958ec1cc5b6a0816a026d76
MehmetBilirr <mehmetbilir@aol.com>
2022-05-25 00:58:40 +0300

    update func added

--- ContactsApp-Firebase/ContactsApp/VC/UpdateContactVC.swift
+++ ContactsApp-Firebase/ContactsApp/VC/UpdateContactVC.swift
@@ -6,20 +6,45 @@
 //
 
 import UIKit
+import Firebase
 
 class UpdateContactVC: UIViewController {
 
     @IBOutlet weak var contactNumberText: UITextField!
     @IBOutlet weak var contactNameText: UITextField!
     var chosenContact : Contacts?
+    var firestoreDB = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        if let contact = chosenContact{
+            contactNameText.text = contact.kisi_ad
+            contactNumberText.text = contact.kisi_tel
+        }
+       
     }
     
 
     @IBAction func updateClicked(_ sender: Any) {
+        
+        if let contact = chosenContact,let name = contactNameText.text,let number = contactNumberText.text  {
+            updateContact(contactID: contact.kisi_id, contactName: name, contactNumber: number)
+        }
+    }
+    
+    func updateContact(contactID:String,contactName:String,contactNumber:String) {
+        
+        let data = ["contact_name":contactName,"contact_number":contactNumber]
+        
+        firestoreDB.collection("contacts").document(contactID).setData(data) { error in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+        
     }
     
 
